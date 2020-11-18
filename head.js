874b1e11ed0627fd12a44f20f8f60c1fe9e6a450
gamma-02 <dposselt24@pilgrim-school.org>
2020-11-18 09:33:57 -0800

    Commited, Basis complete

---
+++ head.js
@@ -0,0 +1,3 @@
+function head() {
+  document.getElementById('head').innerHTML= '<div class="container text-center"><a class="btn btn-info" href="index.html"><h1><small><span class="fas fa-home"></span> Home</small></h1></a></div>'
+}
